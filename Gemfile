fe268e480206b379ee991a32528d4ace74478dd1
Federico Ramallo <framallo@gmail.com>
2011-06-25 12:31:40 -0500

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.9'
+gem 'pg'
+# Use unicorn as the web server
+gem 'unicorn'
+
+# To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
+gem 'ruby-debug'
+# gem 'ruby-debug19', :require => 'ruby-debug'
+
+group :development, :test do
+  gem 'ruby-debug'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Excedent::Application.config.secret_token = 'a499901d3bd2fde376e3ac33756eb3f55c921673103db121a4da9791d31f2b62c444436e11aacb40d7000f11e673e2e4cd5371582d30bc099028666c5b9ab6a5'
